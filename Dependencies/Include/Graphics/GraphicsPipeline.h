ab61ce8b99d65c13f4eca7242f71c927f4ebb2d5
Stefan Toma <34220105+Stefan9283@users.noreply.github.com>
2021-09-19 16:05:26 +0300

    GraphicsPipeline WIP

--- Dependencies/Include/Graphics/GraphicsPipeline.h
+++ Dependencies/Include/Graphics/GraphicsPipeline.h
@@ -4,8 +4,25 @@
 #include "Common.h"
 
 namespace Umbra2D::Graphics {
+    struct RenderNode {
+        RenderPass* rp;
+        int index = -1;
+        RenderPass* fst = nullptr, *snd = nullptr;
+        /* at first fst and snd are null
+         * at runtime based on renderpasstype
+         * the following pointers should be null
+         * - void  - fst = snd = null
+         * - FrBuf - snd = null
+         * - Adder - fst != null && snd != null
+        */
+        void gui();
+    };
     class GraphicsPipeline {
+        std::vector<RenderNode*> renderNodes;
     public:
+        // TODO should use ImNodes
+        void gui();
+        
     };
 }
 
